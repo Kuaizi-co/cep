d478b119fb214763cf8997defb5027b9aa1838a5
kz-fe <shaojinhong@kuaizi.co>
2019-02-22 16:11:10 +0800

    docs: add

---
+++ docs/.vuepress/components/browser-window.vue
@@ -0,0 +1,13 @@
+
+<script>
+import { BrowserWindow } from 'vue-windows'
+export default {
+  extends: BrowserWindow,
+  created () {
+    console.log('window', BrowserWindow)
+  }
+}
+</script>
+
+
+<style src="vue-windows/dist/vue-windows.css"></style>
